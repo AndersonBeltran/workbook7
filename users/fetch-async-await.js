ecb10e7fac5489c83327ce9e8aaa2f78f611d9b9
AndersonBeltran <abeltran@appdev.yearup.org>
2024-11-19 15:45:41 -0500

    Add some JS

--- users/fetch-async-await.js
+++ users/fetch-async-await.js
@@ -1,11 +1,12 @@
 
 
-"use sticts"
+"use stict"
 //only use async in the function when you using await inside the funtion
 async function getData() {
   let promise = fetch("https://jsonplaceholder.typicode.com/users");
   let response = await promise;
-  console.log(response);
+  let data = await response.json(); 
+  console.log(data);
 }
 getData();
 
