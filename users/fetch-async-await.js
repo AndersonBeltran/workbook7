25013542fce49b794c738710861077511c18a631
AndersonBeltran <abeltran@appdev.yearup.org>
2024-11-19 14:12:06 -0500

    add todo

--- users/fetch-async-await.js
+++ users/fetch-async-await.js
@@ -1,6 +1,13 @@
+
+
+"use sticts"
+//only use async in the function when you using await inside the funtion
 async function getData() {
   let promise = fetch("https://jsonplaceholder.typicode.com/users");
   let response = await promise;
   console.log(response);
 }
 getData();
+
+
+
