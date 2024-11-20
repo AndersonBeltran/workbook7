076b18c60f93a94ed179d00ebf442a9c77a04b1c
AndersonBeltran <abeltran@appdev.yearup.org>
2024-11-20 15:14:38 -0500

    Add star wars api and organise the files

--- exercise/scripts/todo.js
+++ exercise/scripts/todo.js
@@ -1,20 +1,20 @@
 "use strict";
 let idInput = document.querySelector("#idInput");
+let detailsDiv = document.querySelector("#detailsDiv");
 
 async function getToDo(id) {
   let promise = fetch("https://jsonplaceholder.typicode.com/todos/" + id);
   let response = await promise;
   let todo = await response.json();
+  detailsDiv.innerText = todo.title;
   console.log(todo);
 }
 function showDetails() {
-  console.log(idInput.value);
+  //   console.log(idInput.value);
   let id = idInput.value;
   getToDo(id);
 }
 
-
-let id = 1
-let message = `Hello, World! ${id}`;
-console.log(message);
-
+// let id = 1;
+// let message = `Hello, World! ${id}`;
+// console.log(message);
